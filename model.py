8f0c6c328d61837b3417fdaa75e8f53c044974b2
Tina Torabi <tntorabii@gmail.com>
2024-04-20 16:50:57 -0700

    Update model.py

--- model.py
+++ model.py
@@ -64,7 +64,7 @@ class PixelCNN(nn.Module):
         self.nr_logistic_mix = nr_logistic_mix
         self.right_shift_pad = nn.ZeroPad2d((1, 0, 0, 0))
         self.down_shift_pad  = nn.ZeroPad2d((0, 0, 1, 0))
-        self.label_embedding = nn.Embedding(num_classes, 8*8*40)
+        self.label_embedding = nn.Embedding(num_classes, 32*32*3)
 
         down_nr_resnet = [nr_resnet] + [nr_resnet + 1] * 2
         self.down_layers = nn.ModuleList([PixelCNNLayer_down(down_nr_resnet[i], nr_filters,
@@ -99,11 +99,11 @@ class PixelCNN(nn.Module):
 
 
     def forward(self, x, labels=None, sample=False):
-        # if labels is not None:
-        #   B, C, H, W = x.shape
-        #   label_emb = self.label_embedding(labels)
-        #   label_emb = label_emb.view(B, 3, 32 ,32)
-        #   x = x + label_emb
+        if labels is not None:
+          B, C, H, W = x.shape
+          label_emb = self.label_embedding(labels)
+          label_emb = label_emb.view(B, 3, 32 ,32)
+          x = x + label_emb
         # similar as done in the tf repo :
         if self.init_padding is not sample:
             xs = [int(y) for y in x.size()]
@@ -134,11 +134,6 @@ class PixelCNN(nn.Module):
         ###    DOWN PASS    ###
         u  = u_list.pop()
         ul = ul_list.pop()
-        if labels is not None:
-            B, C, H, W = ul.shape
-            label_emb = self.label_embedding(labels)
-            label_emb = label_emb.view(B, 40, 8 ,8)
-            ul = ul + label_emb
 
         for i in range(3):
             # resnet block
