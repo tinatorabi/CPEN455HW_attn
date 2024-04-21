87cccc1a694adfae95079602b05b5bf94c5d0cce
Tina Torabi <tntorabii@gmail.com>
2024-04-21 13:50:43 -0700

    Update model.py

--- model.py
+++ model.py
@@ -54,62 +54,98 @@ class PixelCNN(nn.Module):
     def __init__(self, nr_resnet=5, nr_filters=80, nr_logistic_mix=10,
                     resnet_nonlinearity='concat_elu', input_channels=3, num_classes=4):
         super(PixelCNN, self).__init__()
-        if resnet_nonlinearity == 'concat_elu':
-            self.resnet_nonlinearity = lambda x: concat_elu(x)
-        else:
+        if resnet_nonlinearity == 'concat_elu' :
+            self.resnet_nonlinearity = lambda x : concat_elu(x)
+        else :
             raise Exception('right now only concat elu is supported as resnet nonlinearity.')
 
         self.nr_filters = nr_filters
         self.input_channels = input_channels
         self.nr_logistic_mix = nr_logistic_mix
         self.label_embedding = nn.Embedding(num_classes, nr_filters * 32 * 32)
+        self.right_shift_pad = nn.ZeroPad2d((1, 0, 0, 0))
+        self.down_shift_pad  = nn.ZeroPad2d((0, 0, 1, 0))
 
-        self.u_init = down_shifted_conv2d(input_channels + 1, nr_filters, filter_size=(2,3), shift_output_down=True)
-        self.ul_init = down_right_shifted_conv2d(input_channels + 1, nr_filters, filter_size=(2,1), shift_output_right=True)
+        down_nr_resnet = [nr_resnet] + [nr_resnet + 1] * 2
+        self.down_layers = nn.ModuleList([PixelCNNLayer_down(down_nr_resnet[i], nr_filters,
+                                                self.resnet_nonlinearity) for i in range(3)])
 
-        # Setup the resnet blocks
-        self.down_layers = nn.ModuleList([PixelCNNLayer_down(nr_resnet, nr_filters, self.resnet_nonlinearity) for _ in range(3)])
-        self.up_layers = nn.ModuleList([PixelCNNLayer_up(nr_resnet, nr_filters, self.resnet_nonlinearity) for _ in range(3)])
+        self.up_layers   = nn.ModuleList([PixelCNNLayer_up(nr_resnet, nr_filters,
+                                                self.resnet_nonlinearity) for _ in range(3)])
 
-        # Downsampling and upsampling streams
-        self.downsize_u_stream = nn.ModuleList([down_shifted_conv2d(nr_filters, nr_filters, stride=(2,2)) for _ in range(2)])
-        self.downsize_ul_stream = nn.ModuleList([down_right_shifted_conv2d(nr_filters, nr_filters, stride=(2,2)) for _ in range(2)])
-        self.upsize_u_stream = nn.ModuleList([down_shifted_deconv2d(nr_filters, nr_filters, stride=(2,2)) for _ in range(2)])
-        self.upsize_ul_stream = nn.ModuleList([down_right_shifted_deconv2d(nr_filters, nr_filters, stride=(2,2)) for _ in range(2)])
+        self.downsize_u_stream  = nn.ModuleList([down_shifted_conv2d(nr_filters, nr_filters,
+                                                    stride=(2,2)) for _ in range(2)])
 
-        self.nin_out = nin(nr_filters, nr_logistic_mix * 10)  # adjust number_mix based on actual application
+        self.downsize_ul_stream = nn.ModuleList([down_right_shifted_conv2d(nr_filters,
+                                                    nr_filters, stride=(2,2)) for _ in range(2)])
+
+        self.upsize_u_stream  = nn.ModuleList([down_shifted_deconv2d(nr_filters, nr_filters,
+                                                    stride=(2,2)) for _ in range(2)])
+
+        self.upsize_ul_stream = nn.ModuleList([down_right_shifted_deconv2d(nr_filters,
+                                                    nr_filters, stride=(2,2)) for _ in range(2)])
+
+        self.u_init = down_shifted_conv2d(input_channels + 1, nr_filters, filter_size=(2,3),
+                        shift_output_down=True)
+
+        self.ul_init = nn.ModuleList([down_shifted_conv2d(input_channels + 1, nr_filters,
+                                            filter_size=(1,3), shift_output_down=True),
+                                       down_right_shifted_conv2d(input_channels + 1, nr_filters,
+                                            filter_size=(2,1), shift_output_right=True)])
+
+        num_mix = 3 if self.input_channels == 1 else 10
+        self.nin_out = nin(nr_filters, num_mix * nr_logistic_mix)
         self.init_padding = None
 
-    def forward(self, x, labels=None, sample=False):
-        if self.init_padding is None:
-            self.init_padding = Variable(torch.ones(x.shape[0], 1, x.shape[2], x.shape[3]), requires_grad=False)
-            self.init_padding = self.init_padding.cuda() if x.is_cuda else self.init_padding
 
-        # Initial transformations
-        u = self.u_init(torch.cat((x, self.init_padding), 1))
-        ul = self.ul_init(torch.cat((x, self.init_padding), 1))
+    def forward(self, x, sample=False):
+        # similar as done in the tf repo :
+        if self.init_padding is not sample:
+            xs = [int(y) for y in x.size()]
+            padding = Variable(torch.ones(xs[0], 1, xs[2], xs[3]), requires_grad=False)
+            self.init_padding = padding.cuda() if x.is_cuda else padding
 
-        # Embed and reshape labels if provided
-        if labels is not None:
-            label_emb = self.label_embedding(labels)
-            label_emb = label_emb.view(x.shape[0], self.nr_filters, 32, 32)
-            label_emb = F.interpolate(label_emb, size=(u.shape[2], u.shape[3]), mode='nearest')
-            u += label_emb
-            ul += label_emb
+        if sample :
+            xs = [int(y) for y in x.size()]
+            padding = Variable(torch.ones(xs[0], 1, xs[2], xs[3]), requires_grad=False)
+            padding = padding.cuda() if x.is_cuda else padding
+            x = torch.cat((x, padding), 1)
 
-        # Process through the network
-        u_list, ul_list = [u], [ul]
-        for layer in self.up_layers:
-            u, ul = layer(u_list[-1], ul_list[-1])
-            u_list.append(u)
-            ul_list.append(ul)
+        ###      UP PASS    ###
+        x = x if sample else torch.cat((x, self.init_padding), 1)
+        u_list  = [self.u_init(x)]
+        ul_list = [self.ul_init[0](x) + self.ul_init[1](x)]
+        print(len(u_list))
+        print(len(ul_list))
 
-        # Downward pass
-        u, ul = u_list.pop(), ul_list.pop()
-        for layer in self.down_layers:
-            u, ul = layer(u, ul, u_list.pop(), ul_list.pop())
+        for i in range(3):
+            # resnet block
+            u_out, ul_out = self.up_layers[i](u_list[-1], ul_list[-1])
+            u_list  += u_out
+            ul_list += ul_out
+
+            if i != 2:
+                # downscale (only twice)
+                u_list  += [self.downsize_u_stream[i](u_list[-1])]
+                ul_list += [self.downsize_ul_stream[i](ul_list[-1])]
+
+        ###    DOWN PASS    ###
+        u  = u_list.pop()
+        ul = ul_list.pop()
+
+        for i in range(3):
+            # resnet block
+            u, ul = self.down_layers[i](u, ul, u_list, ul_list)
+
+            # upscale (only twice)
+            if i != 2 :
+                u  = self.upsize_u_stream[i](u)
+                ul = self.upsize_ul_stream[i](ul)
 
         x_out = self.nin_out(F.elu(ul))
+
+        assert len(u_list) == len(ul_list) == 0, pdb.set_trace()
+
         return x_out
     
     
